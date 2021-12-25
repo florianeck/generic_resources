39ee7c6243be5468e542656c483ce0f3af981cb8
Florian Eck <florian.eck@el-digital.de>
2021-12-25 02:01:11 +0100

    fallbackback on create DB

--- lib/generic_resources/acts_as_generic_resource.rb
+++ lib/generic_resources/acts_as_generic_resource.rb
@@ -9,26 +9,31 @@ module GenericResources
 
     module ClassMethods
       def acts_as_generic_resource(permitted_attributes: [], overview_attributes: [], deleteable: false)
-        
+
         resource_deleteable = true if deleteable == true
-        
-        if !self.table_exists?
-          Rails.logger.warn "Try to add generic resource #{self.name} - table does not exist"
-          return false
+        begin
+          if !self.table_exists?
+            Rails.logger.warn "Try to add generic resource #{self.name} - table does not exist"
+            return false
+          end
+
+          if permitted_attributes.empty?
+            permitted_attributes = (self.try(:column_names) || []) - ['id', 'created_at', 'updated_at']
+          end
+
+          if overview_attributes.empty?
+            overview_attributes = permitted_attributes
+          end
+
+          GenericResource.register_resource!(self,
+            permitted_attributes: permitted_attributes, overview_attributes: overview_attributes,
+            resource_deleteable: resource_deleteable
+          )
+        rescue ActiveRecord::NoDatabaseError => e
+          puts "SKIPPING acts_as_generic_resource on #{self.name} - NoDatabase"
         end
 
-        if permitted_attributes.empty?
-          permitted_attributes = (self.try(:column_names) || []) - ['id', 'created_at', 'updated_at']
-        end
 
-        if overview_attributes.empty?
-          overview_attributes = permitted_attributes
-        end
-
-        GenericResource.register_resource!(self, 
-          permitted_attributes: permitted_attributes, overview_attributes: overview_attributes,
-          resource_deleteable: resource_deleteable
-        )
       end
     end
 
